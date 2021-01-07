81fb26df6ff366c2842798d96ca7685574415a3c
vannesteromain <romain.vanneste.etu@univ-lille.fr>
2021-01-07 14:46:35 +0100

    Ajout de la prise en charge des fichiers jpeg

--- Back-End/middleware/upload.js
+++ Back-End/middleware/upload.js
@@ -16,7 +16,8 @@ var upload = multer({
     fileFilter : function(req,file,callback){
         if(
             file.mimetype =="image/png" || 
-            file.mimetype =="image/jpg"
+            file.mimetype =="image/jpg" ||
+            file.mimetype =="image/jpeg"
         ){
             callback(null,true)
         }
