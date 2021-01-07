e86f9d9df19377043d8b85e484fd87ef0df0528d
vannesteromain <romain.vanneste.etu@univ-lille.fr>
2021-01-07 15:31:49 +0100

    Ajout de la mise en page des événements

--- Front-End/src/AllEvents.js
+++ Front-End/src/AllEvents.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import moment from 'moment';
 
 class AllEvents extends React.Component{
 
@@ -23,21 +24,35 @@ class AllEvents extends React.Component{
     }
 
     render() {
+        moment.locale('fr');
         return (
-             <div>
+             <div className="row">
+                 <div className="col-md-3 offset-md-9 mb-10">
+                    <a className="btn btn-primary w-100" href={'/addEvent'} > Créer un événement </a>
+                 </div>
                  {this.state.allEvents.map(event => (
-                     <div className="card mt-4">
-                         <div className="card-body">
-                            <h5 className="card-title">{event.name}</h5>
-                            <img src={process.env.PUBLIC_URL + '/' + event.image} />
-                            <ul className="list-unstyled text-left">
-                                <li> Date : {event.date} </li>
-                                <li> Lieu : {event.place}</li>
-                                <li> Code postal : {event.postcode}</li>
-                            </ul>
-                            <a className="btn btn-primary" href={'/updateEvent/' + event._id} > Modifier </a>
-                         </div>
+                     <div className="col-md-12 border border-dark mb-3">
+                        <h2 className="text-center">{event.name}</h2>
+                        <div className="row">
+                            <div className="col-md-4 mb-2">
+                                <img className="imgEvent" src={process.env.PUBLIC_URL + '/' + event.image} />
+                            </div>
+                            <div className="col-md-6">
+                                <ul className="list-unstyled text-left">
+                                    <li> Date : {
+                                        moment(event.date).format("DD MMMM YYYY HH:MM")
+                                    } 
+                                    </li>
+                                    <li> Lieu : {event.place}</li>
+                                    <li> Code postal : {event.postcode}</li>
+                                </ul>
+                            </div>
+                            <div className="col-md-2 mb-3">
+                                <a className="btn btn-primary w-100" href={'/updateEvent/' + event._id} > Modifier </a>
+                            </div>
+                        </div>
                      </div>
+                     
                  ))}
              </div>
         )
