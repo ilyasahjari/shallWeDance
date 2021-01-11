4d5129be6a4759f30e06c7b753ab6d55142790b1
ilyas <56867392+ilyasahjari@users.noreply.github.com>
2021-01-11 16:14:53 +0100

    ajout de la page d'authentification

---
+++ Front-End/src/components/users/Login.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+import {login} from '../services/auth.service'
+
+
+const Login = (props) => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("")
+    const [loading, setLoading] = useState(false)
+    const [errorMessage, setErrorMessage] = useState("")
+
+    const onChangeEmail = (e) => {
+        const email = e.target.value;
+        setEmail(email);
+    };
+
+    const onChangePassword = (e) => {
+        const password = e.target.value;
+        setPassword(password);
+    };
+
+    const handleLogin = (e) =>{
+        e.preventDefault();
+        setLoading(true);
+        try{
+            login(email,password)
+            setLoading(false);
+            props.history.push('/');
+            window.location.reload();
+        }catch(err){
+            setErrorMessage(err);
+            setLoading(false)
+        }
+    }
+
+
+    return (
+        <div>
+            email:
+            <input type="email" onChange={onChangeEmail} /><br />
+          password:
+            <input type="password" onChange={onChangePassword} /><br />
+            <button onClick={handleLogin} >Login</button>
+        </div>
+    )
+}
+
+
+export default Login;
