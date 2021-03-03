f6486e04677c0545f053abbe9d2b460f5e561740
ilyas <56867392+ilyasahjari@users.noreply.github.com>
2021-03-03 12:51:47 +0100

    testing connection heroku

--- server.js
+++ server.js
@@ -46,6 +46,13 @@ app.use(cors())
 
 const PORT = process.env.PORT || 3001
 
+if(process.env.NODE_ENV === 'production'){
+    //Set static folder
+    app.use(express.static('client/build'))
+
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname,'client','build','index.html')))
+}
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
