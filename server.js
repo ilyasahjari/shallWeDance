228847fc6e07ede29898d26c83e5d54a62e1d92e
ilyas <56867392+ilyasahjari@users.noreply.github.com>
2021-03-04 15:44:15 +0100

    test

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ if(process.env.NODE_ENV === 'production'){
     //Set static folder
     app.use(express.static('client/build'))
 
-    app.get('*', (req,res) => res.sendFile(__dirname,'/client/build/index.html'))
+    app.get('*', (req,res) => res.sendFile(__dirname,'client','build','index.html'))
 }
 
 
