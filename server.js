7c7d380c0c9906005e4ef566435632b8dc7e35ae
vannesteromain <romain.vanneste.etu@univ-lille.fr>
2021-03-04 14:32:36 +0100

    Correction erreur path

--- server.js
+++ server.js
@@ -46,12 +46,14 @@ app.use(cors())
 
 const PORT = process.env.PORT || 3001
 
+/*
 if(process.env.NODE_ENV === 'production'){
     //Set static folder
     app.use(express.static('client/build'))
 
     app.get('*', (req,res) => res.sendFile(path.resolve(__dirname,'client','build','index.html')))
 }
+*/
 
 
 app.listen(PORT, () => {
